3292d900c2cc213fc6eafb4563d0d252b564ecd1
Anton von Weltzien <anton.weltzien@camunda.com>
2023-10-11 16:47:39 +0200

    wip add some dummy test

--- src/main/java/io/camunda/connector/inbound/subscription/EmailMessageHandler.java
+++ src/main/java/io/camunda/connector/inbound/subscription/EmailMessageHandler.java
@@ -2,10 +2,15 @@ package io.camunda.connector.inbound.subscription;
 
 import jakarta.mail.BodyPart;
 import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
 import jakarta.mail.Multipart;
+import jakarta.mail.Part;
+import jakarta.mail.internet.MimeBodyPart;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.function.Consumer;
 
@@ -25,23 +30,43 @@ public class EmailMessageHandler {
                 LOG.info("Body " + content);
                 emailBody = new Object[]{content};
             } else if (content instanceof Multipart multiPart) {
+
+
                 ArrayList parts = new ArrayList<Object>();
 
 
-                for (int i = 0; i < multiPart.getCount(); i++) {
-                    BodyPart bodyPart = multiPart.getBodyPart(i);
-                    if (bodyPart.isMimeType("text/plain")) {
-                        parts.add(bodyPart.getContent());
-                        LOG.info("Body {}", bodyPart.getContent());
+
+
+                int partCount = multiPart.getCount();
+                LOG.info("processing multipart message with {} parts", partCount);
+                for (int i = 0; i < partCount; i++) {
+                    MimeBodyPart part = (MimeBodyPart) multiPart.getBodyPart(i);
+
+
+                    if (part.isMimeType("text/plain")) {
+                        LOG.info("mimtype of part {} is text/plain", i+1);
+                        parts.add(part.getContent());
+                        LOG.info("Body {}", part.getContent());
                     }
+                    // store attachments
+                    if (Part.ATTACHMENT.equalsIgnoreCase(part.getDisposition())) {
+                        String file = part.getFileName();
+                        LOG.info("mimtype of part {} is {} and filename is {}", i+1, part.getDisposition(), file);
+                        part.saveFile("." + File.separator + part.getFileName());
+                        parts.add(file);
+                    }
+
+
                 }
                 emailBody = parts.toArray(Object[]::new);
             }
+
 
             EmailWatchServiceSubscriptionEvent ewsse = new EmailWatchServiceSubscriptionEvent(message.getFrom(), message.getReplyTo(), message.getSubject(), emailBody);
             callback.accept(ewsse);
         } catch (Exception e) {
             e.printStackTrace();
+            //todo this should perhaps rethrow so emails can be picket up later again.
         }
 
     }
---
+++ src/test/java/io/camunda/connector/inbound/subscription/EmailMessageHandlerTest.java
@@ -0,0 +1,101 @@
+package io.camunda.connector.inbound.subscription;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import jakarta.mail.*;
+import jakarta.mail.internet.*;
+import jakarta.activation.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+class EmailMessageHandlerTest {
+
+  @BeforeEach
+  void setUp() {
+
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void testShouldParseEmail() {
+    var anEmail = generateMessage();
+
+    EmailMessageHandler.parseEmailAndSendtoCamunda(anEmail, EmailWatchServiceSubscriptionEvent -> {});
+  }
+
+  Message generateMessage() {
+    try {
+      // Create a new text file with some content
+      String textContent = "This is the content of the generated text file.";
+      File textFile = new File("generated_textfile.txt");
+      FileWriter fileWriter = new FileWriter(textFile);
+      fileWriter.write(textContent);
+      fileWriter.close();
+
+      // Create a JavaMail session and message
+      Session session = Session.getDefaultInstance(new Properties());
+      Message message = new MimeMessage(session);
+
+      // Set sender and recipient email addresses
+      message.setFrom(new InternetAddress("sender@example.com"));
+      message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("recipient@example.com"));
+
+      // Set the subject of the email
+      message.setSubject("Sample Email with Attachment");
+
+      // Create a multipart message
+      Multipart multipart = new MimeMultipart();
+
+      // Create and add the text part to the message
+      BodyPart textPart = new MimeBodyPart();
+      textPart.setText("This is the text content of the email.");
+      multipart.addBodyPart(textPart);
+
+      // Create and add the attachment
+      BodyPart attachmentPart = new MimeBodyPart();
+      DataSource source = new FileDataSource(textFile);
+      attachmentPart.setDataHandler(new DataHandler(source));
+      attachmentPart.setFileName(textFile.getName());
+      multipart.addBodyPart(attachmentPart);
+
+      // Set the content of the message to the multipart content
+      message.setContent(multipart);
+
+      // Print the entire message
+      System.out.println("Full Email Message:");
+      message.writeTo(System.out);
+
+
+      return message;
+    } catch (IOException | AddressException e) {
+      e.printStackTrace();
+    } catch (MessagingException e) {
+      throw new RuntimeException(e);
+    }
+    return null;
+  }
+
+}
